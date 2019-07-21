eb87b093e53b1692bdfadb4baebe8abfb80bd5b0
Saima-Chaity <saima.s.chaity@gmail.com>
2019-07-21 13:31:59 -0700

    Added edit todo view and template

--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 from django.template import loader
 from .models import Todo
+from django.utils import timezone
 
 def index(request):
     todo_items = Todo.objects.order_by('-date_created')[:10]
@@ -14,7 +15,17 @@ def index(request):
 def edit(request, todo_id):
     todo = get_object_or_404(Todo, pk=todo_id)
     context = {
+        'todo_id' : todo_id,
         'todo_text' : todo.todo_text,
         'date_created': todo.date_created
     }
     return render(request, 'todos/edit.html', context)
+
+
+def update(request, todo_id):
+
+    edited_item = request.POST['todo_text']
+    todo = get_object_or_404(Todo, pk=todo_id)
+    todo.todo_text = edited_item
+    todo.save()
+    return redirect("/todos")
