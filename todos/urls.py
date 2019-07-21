6230a68a4bfba8d605470b797f498f3f4dc3ad59
Saima-Chaity <saima.s.chaity@gmail.com>
2019-07-21 13:34:25 -0700

    Defined addTodo, edit, update and delete urls

--- todos/urls.py
+++ todos/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-
 from . import views
 
+app_name = 'todos'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('addTodo/', views.addTodo, name='addTodo'),
     path('<int:todo_id>/edit/', views.edit, name='edit'),
+    path('<int:todo_id>/update/', views.update, name='update'),
+    path('<int:todo_id>/delete/', views.delete, name='delete'),
 ]
