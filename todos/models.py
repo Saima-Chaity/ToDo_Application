3ca07150f957c93fb54b9b82cc4efd3f7b4f6761
Saima-Chaity <saima.s.chaity@gmail.com>
2019-07-22 23:21:41 -0700

    Added due date to the model

--- todos/models.py
+++ todos/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
+import datetime
 
 class Todo(models.Model):
     todo_text = models.CharField(max_length=1000)
-    date_created = models.DateTimeField('date_created')
+    date_created = models.DateTimeField(default=datetime.datetime.now)
+    due_date = models.DateTimeField('due_date', null=True)
 
     def __str__(self):
         return self.todo_text
