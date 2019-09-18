c000b3993e63441c9cf87f12a4b2ac72dc910e1d
Saima-Chaity <saima.s.chaity@gmail.com>
2019-09-17 21:51:23 -0700

    Redeploying to azure

--- todo_application/settings.py
+++ todo_application/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'todo-app-python-django.azurewebsites.net'
     'todos.apps.TodosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
