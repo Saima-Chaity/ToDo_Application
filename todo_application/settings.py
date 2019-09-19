34c94ebc6da8442eac105f4e7a2f7c55ed0b8870
Saima-Chaity <saima.s.chaity@gmail.com>
2019-09-18 21:05:40 -0700

    Copied static file for deploying to azure

--- todo_application/settings.py
+++ todo_application/settings.py
@@ -121,6 +121,7 @@ DATE_FORMAT = "mm-dd-yy"
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'todos/static/todos/'
 
 BOOTSTRAP4 = {
     'include_jquery': True,
