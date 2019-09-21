6adf0b66dcfc28e01374ba743a4bb3631655ffcc
Saima-Chaity <saima.s.chaity@gmail.com>
2019-09-21 12:57:14 -0700

    Added dateutil module in the requirements.txt

--- todo_application/settings.py
+++ todo_application/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'todos.apps.TodosConfig',
+    'bootstrap_datepicker_plus',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
